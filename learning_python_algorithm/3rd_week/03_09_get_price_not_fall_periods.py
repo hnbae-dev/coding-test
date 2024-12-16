a0a2fa70ac05d0b638bdd01a7af794cfd444d0e4
hnbae-dev <hnbae.dev@gmail.com>
2024-12-16 20:28:00 +0900

    3/5 - queue

---
+++ learning_python_algorithm/3rd_week/03_09_get_price_not_fall_periods.py
@@ -0,0 +1,24 @@
+prices = [1, 2, 3, 2, 3]
+
+
+def get_price_not_fall_periods(prices):
+    answer = [0] * len(prices)
+    
+    for i in range(len(prices) - 1):
+        for j in range(i + 1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
+    
+    return answer
+
+
+print(get_price_not_fall_periods(prices))
+
+print("정답 = [4, 3, 1, 1, 0] / 현재 풀이 값 = ", get_price_not_fall_periods(prices))
+print("정답 = [6, 2, 1, 3, 2, 1, 0] / 현재 풀이 값 = ",
+      get_price_not_fall_periods([3, 9, 9, 3, 5, 7, 2]))
+print("정답 = [6, 1, 4, 3, 1, 1, 0] / 현재 풀이 값 = ",
+      get_price_not_fall_periods([1, 5, 3, 6, 7, 6, 5]))
